6a35bc702d05ed0606f37e6345caa0d179de75a4
Balaji Pandian <balapk@Balajis-MacBook-Pro.local>
2025-03-15 11:25:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeText() {
+    let paragraph = document.querySelector("p");
+    paragraph.innerText = "You clicked the button!";
+    paragraph.style.color = "red";
+}
+
+// New Feature: Change Background Color
+function changeBackground() {
+    document.body.style.backgroundColor = "#e3f2fd";
+}
